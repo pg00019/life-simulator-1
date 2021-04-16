201420024daa6f81cbbc592520a7c3346e04f848
pg00019 <pg343004@gmail.com>
2021-04-17 00:35:43 +0530

    updated js file

---
+++ Downloads/life-simulator-main (1)/life-simulator-main/js/home.js
@@ -0,0 +1,54 @@
+import * as elements from './elements.js'
+import * as config from './config.js'
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  // Eat Meal function
+  const homeMeal = () => {
+    //health
+    config.updateValue(config.health, +2)
+
+    //happiness
+    config.updateValue(config.happiness, +2)
+
+    config.updateTime(1);
+  }
+
+  const homeRest = () => {
+
+    //health
+    config.updateValue(config.health, +2)
+
+    //happiness
+    config.updateValue(config.happiness, +2)
+
+    config.updateTime(1);
+  }
+
+  const homeMeetfamily = () => {
+    //health
+    config.updateValue(config.health, +2)
+
+    //happiness
+    config.updateValue(config.happiness, +2)
+  }
+
+  function homeCleanRoom() {
+    //health
+    config.updateValue(config.health, -3)
+
+    //happiness
+    config.updateValue(config.happiness, +3)
+  }
+
+  document.addEventListener('click', () => {
+    config.disableElement(elements.homeRest, 23, 7)
+    
+  })
+
+  // btngroup click
+  elements.homeMeal.addEventListener("click", homeMeal)
+  elements.homeRest.addEventListener("click", homeRest)
+  elements.homeMeetFamily.addEventListener("click", homeMeetfamily)
+  elements.homeCleanRoom.addEventListener("click", homeCleanRoom)
+})
